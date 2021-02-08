addc5d17bd0c66e7ffe0d28dfb589a54c837f9c0
Saksham Kaushal <sakshamk.710@gmail.com>
2021-02-09 03:08:52 +0530

    Updated gitignore. Made plotter script central to repository, to be used by every chosen task directory.

--- task1/plotter.py
+++ task1/plotter.py
@@ -22,7 +22,7 @@ def q_ii(df):
 	print('Dispersion :')
 	print(df[cols].groupby('dist_category').std())
 
-def q_a(df,columns):
+def q_a(df,columns,directory):
 	# plt.clf()
 	sns.set()
 	# palette = ['#ffd66b','#522d5b']
@@ -33,19 +33,19 @@ def q_a(df,columns):
 	plt.ylabel('Count', fontsize=9)
 	plt.legend(labels=['True distance','Distance retrieved from parallax measurements'], fontsize=9)
 	# plt.show()
-	plt.savefig('./plots/hist_dist_true-retrieved.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
+	plt.savefig(directory+'./plots/hist_dist_true-retrieved.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
 	return
 
-def q_b(df):
+def q_b(df,directory):
 	sns.set()
 	sns.relplot(data=df,x='retrieved_dist',y='retrieved_abs_mag',kind='scatter',color='#827397',s=5,linewidth=0.05,aspect=2)
 	plt.xlabel('Distance retrieved from parallax measurements (in parsecs)', fontsize=9)
 	plt.ylabel('Absolute magnitude retrieved from parallax measurements', fontsize=9)
 	# plt.show()
-	plt.savefig('./plots/scatter_retrieved_dist-abs_mag.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
+	plt.savefig(directory+'./plots/scatter_retrieved_dist-abs_mag.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
 	return
 
-def q_c(df):
+def q_c(df,directory):
 	sns.set()
 	# palette = ['#fff0d7','#4dabab','#1089b1','#baeaf4']
 	palette = sns.cubehelix_palette(4,rot=0.4,dark=0.75,light=0.1)
@@ -55,19 +55,20 @@ def q_c(df):
 	plt.xlabel('Absolute magnitude retrieved from parallax measurements', fontsize=9)
 	plt.ylabel('Count', fontsize=9)
 	plot._legend.set_title('Distance range')
-	plt.savefig('./plots/hist_retrieved_absmag.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
+	plt.savefig(directory+'./plots/hist_retrieved_absmag.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
 	sns.set(rc={'legend.frameon':True})
 	grid= sns.FacetGrid(data=df,col='dist_category',sharey=False,\
 		palette=palette,aspect=2,hue='dist_category',col_wrap=2, margin_titles=True,legend_out=True)
 	grid.map_dataframe(sns.histplot,x='retrieved_abs_mag',legend=True,bins=bins)
 	grid.set_axis_labels('Absolute magnitude retrieved from parallax measurements','Count',fontsize=9)
 	grid.set_titles(col_template="{col_name}", row_template="{row_name}")
-	plt.savefig('./plots/hist_individual_retrieved_absmag.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
+	plt.savefig(directory+'./plots/hist_individual_retrieved_absmag.jpg',bbox_inches='tight',pad_inches=0.5,dpi=480)
 	# plt.show()
 	return
 
 #------------------ Main --------------------------------
 
+directories=['./task1','./task2','./task3','./task4']
 fname = 'LK.dat'
 col_names = ['true_parallax','true_dist','apparent_mag','observed_parallax','parallax_err','retrieved_dist','true_abs_mag','retrieved_abs_mag','photometric_err']
 
@@ -79,8 +80,10 @@ bins = [0,1000,2000,3000,4000]
 categories = ['upto 1kpc distance','1-2kpc distance range','2-3kpc distance range', '3-4kpc distance range']
 df['dist_category']=pd.cut(df['retrieved_dist'],bins=bins,labels=categories)
 
-q_i(df)
-q_ii(df)
-q_a(df,['retrieved_dist','true_dist'])
-q_b(df)
-q_c(df)
+for directory in directories:
+	print(directory,'\n\n')
+	q_i(df)
+	q_ii(df)
+	q_a(df,['retrieved_dist','true_dist'],directory)
+	q_b(df,directory)
+	q_c(df,directory)
